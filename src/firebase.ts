5b7e81968d014058e363d614e5e7b5c74d03ffd3
Mandharagiri S <mandharagiris@gmail.com>
2025-07-06 16:21:24 +0530

    � Fix Firebase config for Google login

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,11 @@
-// src/firebase.ts
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA4vwl_1hSQKhcNmSJ-qdWrBBN7bTOEBF4",
   authDomain: "taskwise-9b04b.firebaseapp.com",
@@ -12,8 +16,6 @@ const firebaseConfig = {
   measurementId: "G-00MVCBMKP9"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider };
+const analytics = getAnalytics(app);
